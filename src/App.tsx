7a729c370d8f5ddf380414e624b6a7537de26f79
lui <luiz.pb@cryptolab.net>
2024-09-07 12:20:01 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import links from './links.js'
+import './App.css'
+
+/* open weather API config */
+const apiKey = "e61aa27d22eb0b30ab346a9f6441d600";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=porto alegre&appid=${apiKey}&units=metric`;
+
+console.log(links)
+function App() {
+  const [weatherName, setWeatherName] = useState('loading ...');
+  const [temp, setTemp] = useState<number>(0);
+  const [icon, setIcon] = useState<any>([]);
+  const [loc, setLoc] = useState('loading ...');
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Weather Icon
+        //weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        setIcon(
+          `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        );
+
+        // Weather name
+        setWeatherName(data.weather[0].description);
+
+        // Temperature - math.round so the number isn't fractional
+        setTemp(Math.round(data.main.temp));
+        setLoc(data.name);
+      })
+
+  }, [])
+
+  return (
+    <>
+      <div className="wrapper flex flex-col pt-40 items-center h-screen">
+        <div className="weather-container flex flex-col justify-center items-center w-fit text-center border-2 rounded-lg p-3 backdrop-blur-sm">
+          <img src={icon} alt="" className="weather-icon" />
+          <h3>{loc}</h3>
+          <h4 className="weather-name m-0">{weatherName}</h4>
+          <p>{temp}°C</p>
+        </div>
+
+        <div className="links-container flex mt-20 backdrop-blur-sm p-3 pt-0 border-2 rounded-lg">
+          <div className="links-section">
+            <h2 className="section-title">dev</h2>
+            {links.dev.map((i: any) => (
+              <a target='_blank' key={i.key} href={i.url}>{i.title}</a>
+            ))}
+          </div>
+          <div className="links-section">
+            <h2 className="section-title">the web</h2>
+            {links.web.map((i: any) => (
+              <a target='_blank' key={i.key} href={i.url}>{i.title}</a>
+            ))}
+          </div>
+          <div className="links-section">
+            <h2 className="section-title">personal</h2>
+            {links.personal.map((i: any) => (
+              <a target='_blank' key={i.key} href={i.url}>{i.title}</a>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/links.js
@@ -0,0 +1,93 @@
+const urls = {
+    dev: [
+        {
+            key: '1',
+            title: "outlook",
+            url: "https://outlook.office.com/mail/"
+        },
+        {
+            key: '2',
+            title: 'sharepoint',
+            url: 'https://senacrs365.sharepoint.com/sites/FS-GETE-MATERIAIS/Documentos%20Compartilhados/Forms/AllItems.aspx'
+        },
+        {
+            key: '3',
+            title: 'intranet',
+            url: 'https://novaintranet.senacrs.com.br/senac/'
+        },
+        {
+            key: '4',
+            title: 'urledin',
+            url: 'https://urledin.com/'
+        },
+        {
+            key: '5',
+            title: 'github',
+            url: 'https://github.com/'
+        },
+        {
+            key: '6',
+            title: 'leetcode',
+            url: 'https://leetcode.com/'
+        }
+    ],
+    web: [
+        {
+            key: '7',
+            title: 'reddit',
+            url: 'https://old.reddit.com'
+        },
+        {
+            key: '8',
+            title: 'r/brdev',
+            url: 'https://old.reddit.com/r/brdev'
+        },
+        {
+            key: '9',
+            title: 'bsky',
+            url: 'https://bsky.app'
+        },
+        {
+            key: '10',
+            title: 'tabnews',
+            url: 'https://tabnews.com.br/'
+        },
+        {
+            key: '11',
+            title: 'youtube',
+            url: 'https://www.youtube.com/'
+        },
+        {
+            key: '12',
+            title: 'dev.to',
+            url: 'https://dev.to/'
+        }
+    ],
+    personal: [
+        {
+            key: '13',
+            title: "e-mail",
+            url: "https://autistici.org",
+        },
+        {
+            key: '14',
+            title: "pinterest",
+            url: "https://pinterest.com",
+        },
+        {
+            key: '15',
+            title: "moodle",
+            url: "https://moodle.ufrgs.br/login/login.php",
+            tag: "estudos",
+        },
+        {
+            key: '16',
+            title: "udemy",
+            url: "https://udemy.com/",
+            tag: "estudos",
+        },
+    ]
+}
+
+
+export default urls
